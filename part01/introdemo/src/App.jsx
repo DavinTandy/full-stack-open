ce87072ff970e4f15b56a047313a45ebca952d02
DavinTandy <dav.tandy@gmail.com>
2025-04-26 09:54:43 +0700

    Add exercise unicafe format

--- part01/introdemo/src/App.jsx
+++ part01/introdemo/src/App.jsx
@@ -1,22 +1,27 @@
-const Hello = (props) => {
+import { useState } from 'react'
 
-  console.log(props)
-  return (
-    <div>
-      <p>
+const Display = props => <div>{props.value}</div>
 
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const friends = [ 'Peter', 'Maya']
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
 
   return (
     <div>
-      <p>{friends}</p>
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
