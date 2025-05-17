8ac1606209339f2f4fd7887f27be1871926ecaae
DavinTandy <dav.tandy@gmail.com>
2025-05-17 11:34:56 +0700

    Add exercises 1.9

--- part01/1.6-1.11-unicafe/src/App.jsx
+++ part01/1.6-1.11-unicafe/src/App.jsx
@@ -4,6 +4,24 @@ const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
 const Statistics = (props) => <p>{props.text} {props.value}{props.text2}</p>
 
+const History = (props) => {
+  if (props.all === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+  return (
+    <div>
+      <Statistics text="good" value={props.good} />
+      <Statistics text="neutral" value={props.neutral} />
+      <Statistics text="bad" value={props.bad} />
+      <Statistics text="all" value={props.good + props.neutral + props.bad} />
+      <Statistics text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
+      <Statistics text="positive" value={props.good / (props.good + props.neutral + props.bad) * 100} text2="%" />
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -28,12 +46,7 @@ const App = () => {
       <Button onClick={() => addNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => addBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
-      <Statistics text="good" value={good} />
-      <Statistics text="neutral" value={neutral} />
-      <Statistics text="bad" value={bad} />
-      <Statistics text="all" value={good + neutral + bad} />
-      <Statistics text="average" value={(good - bad) / (good + neutral + bad)} />
-      <Statistics text="positive" value={good / (good + neutral + bad) * 100} text2="%" />
+      <History all={good + neutral + bad} good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
