ad1703898e1882440cc700a30cc632909eb44601
DavinTandy <dav.tandy@gmail.com>
2025-07-21 23:42:31 +0700

    Add exercises 2.5

--- part02/2.1-2.5-course-information/src/App.jsx
+++ part02/2.1-2.5-course-information/src/App.jsx
@@ -1,32 +1,4 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.part} {props.exercises}</p>
-    </div>
-  )
-}
-
-const Course = ({course}) => {
-  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return (
-    <div>
-      <Header course={course.name} />
-      {course.parts.map(part => (
-        <Part key={part.id} part={part.name} exercises={part.exercises} />
-      ))}
-      <p><strong>total of {total} exercises</strong></p>
-    </div>
-  )
-}
+import Course from './course'
 
 const App = () => {
   const courses = [
