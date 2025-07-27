8a49bb1c855217774f8c8651cd2607fb46a0093a
DavinTandy <dav.tandy@gmail.com>
2025-07-27 14:06:53 +0700

    Add exercises 2.6

--- part02/2.6-2.12-phonebook/src/App.jsx
+++ part02/2.6-2.12-phonebook/src/App.jsx
@@ -6,19 +6,42 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addContact = (event) => {
+    event.preventDefault()
+    const newObject = {
+      name: newName,
+      id: String(persons.length + 1)
+    }
+
+    setPersons(persons.concat(newObject))
+    setNewName('')
+  }
+
+  const handleContactChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const Contact = ({ contact }) => {
+    return <li>{contact.name}</li>
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addContact}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleContactChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      
+      <ul>
+        {persons.map(person =>
+          <Contact key={person.id} contact={person} />
+        )}
+      </ul>
     </div>
   )
 }
